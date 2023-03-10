2bf827d729a775c33e8d938a705a5658c9b94d1e
quartersdg <sdgoglin@gmail.com>
2023-03-10 15:36:08 -0800

    first

---
+++ StS-FriendsModBase-master/friendsMod/src/main/java/friendsMod/DefaultMod.java
@@ -0,0 +1,280 @@
+package friendsMod;
+
+import basemod.BaseMod;
+import basemod.ModLabeledToggleButton;
+import basemod.ModPanel;
+import basemod.interfaces.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.localization.*;
+import friendsMod.util.IDCheckDontTouchPls;
+import friendsMod.util.TextureLoader;
+import friendsMod.variables.DefaultCustomVariable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static basemod.BaseMod.gson;
+
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+//TODO: DON'T MASS RENAME/REFACTOR
+// Please don't just mass replace "theDefault" with "yourMod" everywhere.
+// It'll be a bigger pain for you. You only need to replace it in 3 places.
+// I comment those places below, under the place where you set your ID.
+
+//TODO: FIRST THINGS FIRST: RENAME YOUR PACKAGE AND ID NAMES FIRST-THING!!!
+// Right click the package (Open the project pane on the left. Folder with black dot on it. The name's at the very top)
+// -> Refactor -> Rename, and name it whatever you wanna call your mod.
+// Scroll down in this file. Change the ID from "theDefault:" to "yourModName:" or whatever your heart desires
+// (don't use spaces). Dw, you'll see it.
+// In the JSON strings (resources>localization>eng>[all them files] make sure they all go "yourModName:" rather than
+// "theDefault". You can ctrl+R to replace in 1 file, or ctrl+shift+r to mass replace in specific files/directories
+// (Be careful.).
+// Start with the DefaultCommon cards - they are the most commented cards since I don't feel it's necessary to put
+// identical comments on every card.
+// After you sorta get the hang of how to make cards, check out the card template which will make your life easier
+
+/*
+ * With that out of the way:
+ * Welcome to this super over-commented Slay the Spire modding base.
+ * Use it to make your own mod of any type. - If you want to add any standard in-game content (character,
+ * cards, relics), this is a good starting point.
+ * It features 1 character with a minimal set of things: 1 card of each type, 1 debuff, couple of relics, etc.
+ * If you're new to modding, you basically *need* the BaseMod wiki for whatever you wish to add
+ * https://github.com/daviscook477/BaseMod/wiki - work your way through with this base.
+ * Feel free to use this in any way you like, of course. MIT licence applies. Happy modding!
+ *
+ * And pls. Read the comments.
+ */
+
+@SpireInitializer
+public class DefaultMod implements EditCardsSubscriber, EditRelicsSubscriber, EditStringsSubscriber,
+        EditKeywordsSubscriber, EditCharactersSubscriber, PostInitializeSubscriber {
+    // Make sure to implement the subscribers *you* are using (read basemod wiki). Editing cards? EditCardsSubscriber.
+    // Making relics? EditRelicsSubscriber. etc., etc., for a full list and how to make your own, visit the basemod
+    // wiki.
+    public static final Logger logger = LogManager.getLogger(DefaultMod.class.getName());
+    private static String modID;
+
+    // Mod-settings settings. This is if you want an on/off savable button
+    public static Properties masterModDefaultSettings = new Properties();
+    public static final String ENABLE_PLACEHOLDER_SETTINGS = "enablePlaceholder";
+    public static boolean enablePlaceholder = true; // The boolean we'll be setting on/off (true/false)
+
+    //This is for the in-game mod settings panel.
+    private static final String MODNAME = "Default Mod";
+    private static final String AUTHOR = "Gremious, sdgoglin"; // And pretty soon - You!
+    private static final String DESCRIPTION = "A mod.";
+
+    // =============== INPUT TEXTURE LOCATION =================
+
+    public static final List<String> friends = new ArrayList<>();
+    public static String highlightColor = "purple";
+
+    public DefaultMod() {
+        logger.info("Subscribe to BaseMod hooks");
+
+        BaseMod.subscribe(this);
+
+        setModID("friendsMod");
+
+        logger.info("Adding mod settings");
+        logger.info("Done adding mod settings");
+
+    }
+
+    // ====== NO EDIT AREA ======
+    // DON'T TOUCH THIS STUFF. IT IS HERE FOR STANDARDIZATION BETWEEN MODS AND TO ENSURE GOOD CODE PRACTICES.
+    // IF YOU MODIFY THIS I WILL HUNT YOU DOWN AND DOWNVOTE YOUR MOD ON WORKSHOP
+
+    public static void setModID(String ID) { // DON'T EDIT
+        Gson coolG = new Gson(); // EY DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf
+        //   (StandardCharsets.UTF_8)); // i hate u Gdx.files
+        InputStream in = DefaultMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT
+        // THIS ETHER
+        IDCheckDontTouchPls EXCEPTION_STRINGS = coolG.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8),
+                IDCheckDontTouchPls.class); // OR THIS, DON'T EDIT IT
+        logger.info("You are attempting to set your mod ID as: " + ID); // NO WHY
+        if (ID.equals(EXCEPTION_STRINGS.DEFAULTID)) { // DO *NOT* CHANGE THIS ESPECIALLY, TO EDIT YOUR MOD ID, SCROLL
+            // UP JUST A LITTLE, IT'S JUST ABOVE
+            throw new RuntimeException(EXCEPTION_STRINGS.EXCEPTION); // THIS ALSO DON'T EDIT
+        } else if (ID.equals(EXCEPTION_STRINGS.DEVID)) { // NO
+            modID = EXCEPTION_STRINGS.DEFAULTID; // DON'T
+        } else { // NO EDIT AREA
+            modID = ID; // DON'T WRITE OR CHANGE THINGS HERE NOT EVEN A LITTLE
+        } // NO
+        logger.info("Success! ID is " + modID); // WHY WOULD U WANT IT NOT TO LOG?? DON'T EDIT THIS.
+    } // NO
+
+    public static String getModID() { // NO
+        return modID; // DOUBLE NO
+    } // NU-UH
+
+    private static void pathCheck() { // ALSO NO
+        Gson coolG = new Gson(); // NNOPE DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf
+        //   (StandardCharsets.UTF_8)); // i still hate u btw Gdx.files
+        InputStream in = DefaultMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT
+        // THISSSSS
+        IDCheckDontTouchPls EXCEPTION_STRINGS = coolG.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8),
+                IDCheckDontTouchPls.class); // NAH, NO EDIT
+        String packageName = DefaultMod.class.getPackage().getName(); // STILL NO EDIT ZONE
+        FileHandle resourcePathExists = Gdx.files.internal(getModID() + "Resources"); // PLEASE DON'T EDIT THINGS
+        // HERE, THANKS
+        if (!modID.equals(EXCEPTION_STRINGS.DEVID)) { // LEAVE THIS EDIT-LESS
+            if (!packageName.equals(getModID())) { // NOT HERE ETHER
+                throw new RuntimeException(EXCEPTION_STRINGS.PACKAGE_EXCEPTION + getModID()); // THIS IS A NO-NO
+            } // WHY WOULD U EDIT THIS
+            if (!resourcePathExists.exists()) { // DON'T CHANGE THIS
+                throw new RuntimeException(EXCEPTION_STRINGS.RESOURCE_FOLDER_EXCEPTION + getModID() + "Resources");
+                // NOT THIS
+            }// NO
+        }// NO
+    }// NO
+
+    // ====== YOU CAN EDIT AGAIN ======
+
+
+    @SuppressWarnings("unused")
+    public static void initialize() {
+        logger.info("========================= Initializing Default Mod. Hi. =========================");
+        DefaultMod defaultmod = new DefaultMod();
+        logger.info("========================= /Default Mod Initialized. Hello World./ =========================");
+    }
+
+    // ============== /SUBSCRIBE, CREATE THE COLOR_GRAY, INITIALIZE/ =================
+
+
+    // =============== LOAD THE CHARACTER =================
+
+    @Override
+    public void receiveEditCharacters() {
+        receiveEditPotions();
+    }
+
+    // =============== /LOAD THE CHARACTER/ =================
+
+
+    // =============== POST-INITIALIZE =================
+
+    @Override
+    public void receivePostInitialize() {
+        logger.info("Loading badge image and mod options");
+
+        logger.info("Done loading badge Image and mod options");
+    }
+
+    // =============== / POST-INITIALIZE/ =================
+
+
+    // ================ ADD POTIONS ===================
+
+    public void receiveEditPotions() {
+        logger.info("Beginning to edit potions");
+
+        logger.info("Done editing potions");
+    }
+
+    // ================ /ADD POTIONS/ ===================
+
+
+    // ================ ADD RELICS ===================
+
+    @Override
+    public void receiveEditRelics() {
+        logger.info("Adding relics");
+
+        logger.info("Done adding relics!");
+    }
+
+    // ================ /ADD RELICS/ ===================
+
+
+    // ================ ADD CARDS ===================
+
+    @Override
+    public void receiveEditCards() {
+        logger.info("Adding variables");
+        //Ignore this
+        pathCheck();
+        // Add the Custom Dynamic Variables
+        logger.info("Add variabls");
+
+        logger.info("Adding cards");
+        // Add the cards
+        // Don't comment out/delete these cards (yet). You need 1 of each type and rarity (technically) for your game
+        // not to crash
+        // when generating card rewards/shop screen items.
+
+        logger.info("Making sure the cards are unlocked.");
+        // Unlock the cards
+        // This is so that they are all "seen" in the library, for people who like to look at the card list
+        // before playing your mod.
+
+        logger.info("Done adding cards!");
+    }
+
+    // There are better ways to do this than listing every single individual card, but I do not want to complicate
+    // things
+    // in a "tutorial" mod. This will do and it's completely ok to use. If you ever want to clean up and
+    // shorten all the imports, go look take a look at other mods, such as Hubris.
+
+    // ================ /ADD CARDS/ ===================
+
+
+    // ================ LOAD THE TEXT ===================
+
+    private static class FriendsStrings {
+        List<String> friends;
+
+        String highlight_color;
+    }
+
+    @Override
+    public void receiveEditStrings() {
+        logger.info("################################################################################");
+
+        String friendsJson = Gdx.files.local("mods/friends.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        FriendsStrings m = gson.fromJson(friendsJson, FriendsStrings.class);
+        logger.info("Mastered: " + String.join(",", m.friends));
+        friends.addAll(m.friends);
+        if (!m.highlight_color.isEmpty()) {
+            highlightColor = m.highlight_color;
+        }
+
+        logger.info("################################################################################");
+
+        logger.info("Done editing strings");
+    }
+
+    // ================ /LOAD THE TEXT/ ===================
+
+    // ================ LOAD THE KEYWORDS ===================
+
+    @Override
+    public void receiveEditKeywords() {
+    }
+
+    // ================ /LOAD THE KEYWORDS/ ===================    
+
+    // this adds "ModName:" before the ID of any card/relic/power etc.
+    // in order to avoid conflicts if any other mod uses the same ID.
+    public static String makeID(String idText) {
+        return getModID() + ":" + idText;
+    }
+}
